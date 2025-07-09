e5f222217ff97f174c8dbea47f46489617bdb034
David Barreto <davidbarretobk@gmail.com>
2025-07-08 22:50:19 -0300

    rotas coringas

--- src/Controller/HelloController.php
+++ src/Controller/HelloController.php
@@ -24,4 +24,18 @@ class HelloController
     {
         return new Response('Tu num blacumeja!');
     }
+
+    // Rotas coringa
+    #[Route('/testando/{slug}')]
+    // public function teste($slug): Response
+    // public function url(string $slug=null): Response // colocar o tipo está em desuso
+    public function url($slug=null): Response // para que não dê erro caso eu não passe nenhuma variável na URL
+    {
+        return new Response('Esse é o meu ' . $slug);
+    }
+
+    // Digito:
+    // /testando/teste1 - "Esse é o meu teste1"
+    // /testando/teste2 - "Esse é o meu teste2"
+    // /testando/teste3 - "Esse é o meu teste3"
 }
